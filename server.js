d5b4d2568624653b273fc6864edb4168c312569a
xristosmpalkopoulos-design <xristosmpalkopoulos@gmail.com>
2026-06-10 22:19:57 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('PPODV Live Server Running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
